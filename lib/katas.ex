0b4be0643a419316d924ca891921abff19cab24a
Rob Keefer <rob.keefer@pomiet.com>
2017-10-27 16:22:11 -0400

    Introduce Linear Search This performs a linear recursive search through the list.

--- lib/katas.ex
+++ lib/katas.ex
@@ -1,18 +1,34 @@
 defmodule Katas do
-  @moduledoc """
-  Documentation for Katas.
-  """
+  # @moduledoc """
+  # Documentation for Katas.
+  # """
+  #
+  # @doc """
+  # Hello world.
+  #
+  # ## Examples
+  #
+  #     iex> Katas.hello
+  #     :world
+  #
+  # """
+  def chop(num, []) do
+    -1
+  end
 
-  @doc """
-  Hello world.
+  def chop(num, [head]) do
+    if num == head do
+      head
+    else
+      -1
+    end
+  end
 
-  ## Examples
-
-      iex> Katas.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def chop(num, [head | tail]) do
+    if num == head do
+      head
+    else
+      chop(num, tail)
+    end
   end
 end
--- test/katas_test.exs
+++ test/katas_test.exs
@@ -1,8 +1,54 @@
 defmodule KatasTest do
   use ExUnit.Case
-  doctest Katas
+  # doctest Katas
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "search finds nothing in empty list" do
+    assert -1 == Katas.chop(3, [])
   end
+
+  test "search finds nothing in list of one" do
+    assert -1 == Katas.chop(3, [1])
+  end
+
+  test "search finds match in list of one" do
+    assert 1 == Katas.chop(1, [1])
+  end
+
+  test "search matches on first value in list of three" do
+    assert 1 == Katas.chop(1, [1, 3, 5])
+  end
+
+  test "search matches on middle value in list of three" do
+    assert 3 == Katas.chop(3, [1, 3, 5])
+  end
+
+  test "search matches on last value in list of three" do
+    assert 5 == Katas.chop(5, [1, 3, 5])
+  end
+
+  test "search matches on first value in list of four" do
+    assert 1 == Katas.chop(1, [1, 3, 5, 7])
+  end
+
+  test "search matches on middle value in list of four" do
+    assert 3 == Katas.chop(3, [1, 3, 5, 7])
+  end
+
+  test "search matches on last value in list of four" do
+    assert 7 == Katas.chop(7, [1, 3, 5, 7])
+  end
+
+  test "search does not match on non-existent numbers, even at edges" do
+    assert -1 == Katas.chop(0, [1, 3, 5])
+    assert -1 == Katas.chop(2, [1, 3, 5])
+    assert -1 == Katas.chop(4, [1, 3, 5])
+    assert -1 == Katas.chop(6, [1, 3, 5])
+
+    assert -1 == Katas.chop(0, [1, 3, 5, 7])
+    assert -1 == Katas.chop(2, [1, 3, 5, 7])
+    assert -1 == Katas.chop(4, [1, 3, 5, 7])
+    assert -1 == Katas.chop(6, [1, 3, 5, 7])
+    assert -1 == Katas.chop(8, [1, 3, 5, 7])
+  end
+
 end
