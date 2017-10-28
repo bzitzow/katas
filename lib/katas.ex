e2fc6efa7ee5c10966a473b5e252b252c3415f9b
Rob Keefer <rob.keefer@pomiet.com>
2017-10-27 20:07:09 -0400

    Add binary search Add a solution to a binary recursive search kata

--- lib/katas.ex
+++ lib/katas.ex
@@ -1,34 +1,57 @@
 defmodule Katas do
-  # @moduledoc """
-  # Documentation for Katas.
-  # """
-  #
-  # @doc """
-  # Hello world.
-  #
-  # ## Examples
-  #
-  #     iex> Katas.hello
-  #     :world
-  #
-  # """
-  def chop(num, []) do
-    -1
+  @moduledoc """
+  These are solutions to common katas.
+  
+  @doc
+  Katas implmented to date include:
+    lin_search: a linear recursive search
+    chop: a binary recursive search
+
+  ## Examples
+
+      iex> Katas.lin_search(1, [1, 3, 5, 7])
+      1
+
+      iex> Katas.chop(1, [1, 3, 5, 7])
+      1
+
+  """
+  def lin_search(num, []) do
+    :notfound
   end
 
-  def chop(num, [head]) do
+  def lin_search(num, [head | tail]) do
     if num == head do
       head
     else
-      -1
+      lin_search(num, tail)
     end
   end
 
-  def chop(num, [head | tail]) do
-    if num == head do
-      head
+  def chop(num, []) do
+    :notfound
+  end
+
+  def chop(num, num_list) when 1 == length(num_list) do
+    if (List.first(num_list) == num) do
+      num
     else
-      chop(num, tail)
+      :notfound
+    end
+  end
+
+  def chop(num, num_list) do
+    midpoint = ((length(num_list)/2) |> round)
+    element = Enum.at(num_list, midpoint)
+    if (element == num) do
+      num
+    else
+      [left | right] = Enum.chunk(num_list, midpoint, midpoint, [])
+      if (element > num) do
+       chop(num, List.flatten(left))
+      else
+        chop(num, List.flatten(right))
+      end
     end
   end
 end
--- test/katas_test.exs
+++ test/katas_test.exs
@@ -3,52 +3,72 @@ defmodule KatasTest do
   # doctest Katas
 
   test "search finds nothing in empty list" do
-    assert -1 == Katas.chop(3, [])
+    assert :notfound == Katas.lin_search(3, [])
+    assert :notfound == Katas.chop(3, [])
   end
 
   test "search finds nothing in list of one" do
-    assert -1 == Katas.chop(3, [1])
+    assert :notfound == Katas.lin_search(3, [1])
+    assert :notfound == Katas.chop(3, [1])
   end
 
   test "search finds match in list of one" do
+    assert 1 == Katas.lin_search(1, [1])
     assert 1 == Katas.chop(1, [1])
   end
 
   test "search matches on first value in list of three" do
+    assert 1 == Katas.lin_search(1, [1, 3, 5])
     assert 1 == Katas.chop(1, [1, 3, 5])
   end
 
   test "search matches on middle value in list of three" do
+    assert 3 == Katas.lin_search(3, [1, 3, 5])
     assert 3 == Katas.chop(3, [1, 3, 5])
   end
 
   test "search matches on last value in list of three" do
+    assert 5 == Katas.lin_search(5, [1, 3, 5])
     assert 5 == Katas.chop(5, [1, 3, 5])
   end
 
   test "search matches on first value in list of four" do
+    assert 1 == Katas.lin_search(1, [1, 3, 5, 7])
     assert 1 == Katas.chop(1, [1, 3, 5, 7])
   end
 
   test "search matches on middle value in list of four" do
+    assert 3 == Katas.lin_search(3, [1, 3, 5, 7])
     assert 3 == Katas.chop(3, [1, 3, 5, 7])
   end
 
   test "search matches on last value in list of four" do
+    assert 7 == Katas.lin_search(7, [1, 3, 5, 7])
     assert 7 == Katas.chop(7, [1, 3, 5, 7])
   end
 
   test "search does not match on non-existent numbers, even at edges" do
-    assert -1 == Katas.chop(0, [1, 3, 5])
-    assert -1 == Katas.chop(2, [1, 3, 5])
-    assert -1 == Katas.chop(4, [1, 3, 5])
-    assert -1 == Katas.chop(6, [1, 3, 5])
+    assert :notfound == Katas.lin_search(0, [1, 3, 5])
+    assert :notfound == Katas.lin_search(2, [1, 3, 5])
+    assert :notfound == Katas.lin_search(4, [1, 3, 5])
+    assert :notfound == Katas.lin_search(6, [1, 3, 5])
 
-    assert -1 == Katas.chop(0, [1, 3, 5, 7])
-    assert -1 == Katas.chop(2, [1, 3, 5, 7])
-    assert -1 == Katas.chop(4, [1, 3, 5, 7])
-    assert -1 == Katas.chop(6, [1, 3, 5, 7])
-    assert -1 == Katas.chop(8, [1, 3, 5, 7])
+    assert :notfound == Katas.chop(0, [1, 3, 5])
+    assert :notfound == Katas.chop(2, [1, 3, 5])
+    assert :notfound == Katas.chop(4, [1, 3, 5])
+    assert :notfound == Katas.chop(6, [1, 3, 5])
+
+    assert :notfound == Katas.lin_search(0, [1, 3, 5, 7])
+    assert :notfound == Katas.lin_search(2, [1, 3, 5, 7])
+    assert :notfound == Katas.lin_search(4, [1, 3, 5, 7])
+    assert :notfound == Katas.lin_search(6, [1, 3, 5, 7])
+    assert :notfound == Katas.lin_search(8, [1, 3, 5, 7])
+
+    assert :notfound == Katas.chop(0, [1, 3, 5, 7])
+    assert :notfound == Katas.chop(2, [1, 3, 5, 7])
+    assert :notfound == Katas.chop(4, [1, 3, 5, 7])
+    assert :notfound == Katas.chop(6, [1, 3, 5, 7])
+    assert :notfound == Katas.chop(8, [1, 3, 5, 7])
   end
 
 end
