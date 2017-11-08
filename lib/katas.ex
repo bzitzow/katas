740ea86e0ba357222e22119e1888e37ebd05f442
Rob Keefer <rob.keefer@pomiet.com>
2017-11-08 09:04:46 -0500

    Add Jump Search and begin to add Interpolation Search
    Playing with different search algorithms as katas.

--- lib/katas.ex
+++ lib/katas.ex
@@ -1,7 +1,7 @@
 defmodule Katas do
   @moduledoc """
   These are solutions to common katas.
-  
+
   @doc
   Katas implmented to date include:
     lin_search: a linear recursive search
@@ -12,7 +12,7 @@ defmodule Katas do
       iex> Katas.lin_search(1, [1, 3, 5, 7])
       1
 
-      iex> Katas.chop(1, [1, 3, 5, 7])
+      iex> Katas.bin_search(1, [1, 3, 5, 7])
       1
 
   """
@@ -28,11 +28,11 @@ defmodule Katas do
     end
   end
 
-  def chop(num, []) do
+  def bin_search(num, []) do
     :notfound
   end
 
-  def chop(num, num_list) when 1 == length(num_list) do
+  def bin_search(num, num_list) when 1 == length(num_list) do
     if (List.first(num_list) == num) do
       num
     else
@@ -40,18 +40,84 @@ defmodule Katas do
     end
   end
 
-  def chop(num, num_list) do
+  def bin_search(num, num_list) do
     midpoint = ((length(num_list)/2) |> round)
     element = Enum.at(num_list, midpoint)
-    if (element == num) do
-      num
-    else
-      [left | right] = Enum.chunk(num_list, midpoint, midpoint, [])
-      if (element > num) do
-       chop(num, List.flatten(left))
-      else
-        chop(num, List.flatten(right))
-      end
+    [left | right] = Enum.chunk(num_list, midpoint, midpoint, [])
+
+    cond do
+      (element == num) ->
+        num
+      (element > num) ->
+        bin_search(num, List.flatten(left))
+      true ->
+        bin_search(num, List.flatten(right))
     end
   end
+
+  def jmp_search(num, []) do
+    :notfound
+  end
+
+  def jmp_search(num, num_list) when 1 == length(num_list) do
+    if (List.first(num_list) == num) do
+      num
+    else
+      :notfound
+    end
+  end
+
+  def jmp_search(num, num_list) do
+    jump = :math.sqrt(length(num_list)) |> round
+    element = Enum.at(num_list, jump)
+    [left | right] = Enum.chunk(num_list, jump, jump, [])
+
+    cond do
+      (element == num) ->
+        num
+      (element > num) ->
+        jmp_search(num, List.flatten(left))
+      true ->
+        jmp_search(num, List.flatten(right))
+    end
+  end
+
+  def interpolation_search(num, []) do
+    :notfound
+  end
+
+  def interpolation_search(num, num_list) when 1 == length(num_list) do
+    if (List.first(num_list) == num) do
+      num
+    else
+      :notfound
+    end
+  end
+
+  def interpolation_search(num, num_list) do
+    if ((num < List.first(num_list)) || (num > List.last(num_list))) do
+      :notfound
+    else
+    #    pos = lo + [ (x-arr[lo])*(hi-lo) / (arr[hi]-arr[Lo]) ]
+    pos = 1 + ( (num - List.first(num_list)) * (length(num_list) - 1) /
+      (List.last(num_list) - List.first(num_list))) |> round
+
+    element = Enum.at(num_list, pos-1)
+    [left | right] = Enum.chunk(num_list, pos, pos, [])
+
+    cond do
+      (List.first(num_list) == num) ->
+        num
+      (List.last(num_list) == num) ->
+        num
+      (element == num) ->
+        num
+      (element > num) ->
+        interpolation_search(num, List.flatten(left))
+      true ->
+        interpolation_search(num, List.flatten(right))
+    end
+  end
+end
+
 end
--- test/katas_test.exs
+++ test/katas_test.exs
@@ -4,47 +4,64 @@ defmodule KatasTest do
 
   test "search finds nothing in empty list" do
     assert :notfound == Katas.lin_search(3, [])
-    assert :notfound == Katas.chop(3, [])
+    assert :notfound == Katas.bin_search(3, [])
+    assert :notfound == Katas.jmp_search(3, [])
   end
 
   test "search finds nothing in list of one" do
     assert :notfound == Katas.lin_search(3, [1])
-    assert :notfound == Katas.chop(3, [1])
+    assert :notfound == Katas.bin_search(3, [1])
+    assert :notfound == Katas.jmp_search(3, [1])
   end
 
   test "search finds match in list of one" do
     assert 1 == Katas.lin_search(1, [1])
-    assert 1 == Katas.chop(1, [1])
+    assert 1 == Katas.bin_search(1, [1])
+    assert 1 == Katas.jmp_search(1, [1])
   end
 
   test "search matches on first value in list of three" do
     assert 1 == Katas.lin_search(1, [1, 3, 5])
-    assert 1 == Katas.chop(1, [1, 3, 5])
+    assert 1 == Katas.bin_search(1, [1, 3, 5])
+    assert 1 == Katas.jmp_search(1, [1, 3, 5])
   end
 
   test "search matches on middle value in list of three" do
     assert 3 == Katas.lin_search(3, [1, 3, 5])
-    assert 3 == Katas.chop(3, [1, 3, 5])
+    assert 3 == Katas.bin_search(3, [1, 3, 5])
+    assert 3 == Katas.jmp_search(3, [1, 3, 5])
   end
 
   test "search matches on last value in list of three" do
     assert 5 == Katas.lin_search(5, [1, 3, 5])
-    assert 5 == Katas.chop(5, [1, 3, 5])
+    assert 5 == Katas.bin_search(5, [1, 3, 5])
+    assert 5 == Katas.jmp_search(5, [1, 3, 5])
   end
 
   test "search matches on first value in list of four" do
     assert 1 == Katas.lin_search(1, [1, 3, 5, 7])
-    assert 1 == Katas.chop(1, [1, 3, 5, 7])
+    assert 1 == Katas.bin_search(1, [1, 3, 5, 7])
+    assert 1 == Katas.jmp_search(1, [1, 3, 5, 7])
   end
 
   test "search matches on middle value in list of four" do
     assert 3 == Katas.lin_search(3, [1, 3, 5, 7])
-    assert 3 == Katas.chop(3, [1, 3, 5, 7])
+    assert 3 == Katas.bin_search(3, [1, 3, 5, 7])
+    assert 3 == Katas.jmp_search(3, [1, 3, 5, 7])
+  end
+
+  test "search matches on middle value in long list" do
+    assert 55 == Katas.lin_search(55, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610])
+    assert 55 == Katas.bin_search(55, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610])
+    assert 55 == Katas.jmp_search(55, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610])
+    # assert 55 == Katas.interpolation_search(55, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610])
   end
 
   test "search matches on last value in list of four" do
     assert 7 == Katas.lin_search(7, [1, 3, 5, 7])
-    assert 7 == Katas.chop(7, [1, 3, 5, 7])
+    assert 7 == Katas.bin_search(7, [1, 3, 5, 7])
+    assert 7 == Katas.jmp_search(7, [1, 3, 5, 7])
+    # assert 7 == Katas.interpolation_search(7, [1, 3, 5, 7])
   end
 
   test "search does not match on non-existent numbers, even at edges" do
@@ -53,10 +70,20 @@ defmodule KatasTest do
     assert :notfound == Katas.lin_search(4, [1, 3, 5])
     assert :notfound == Katas.lin_search(6, [1, 3, 5])
 
-    assert :notfound == Katas.chop(0, [1, 3, 5])
-    assert :notfound == Katas.chop(2, [1, 3, 5])
-    assert :notfound == Katas.chop(4, [1, 3, 5])
-    assert :notfound == Katas.chop(6, [1, 3, 5])
+    assert :notfound == Katas.bin_search(0, [1, 3, 5])
+    assert :notfound == Katas.bin_search(2, [1, 3, 5])
+    assert :notfound == Katas.bin_search(4, [1, 3, 5])
+    assert :notfound == Katas.bin_search(6, [1, 3, 5])
+
+    assert :notfound == Katas.jmp_search(0, [1, 3, 5])
+    assert :notfound == Katas.jmp_search(2, [1, 3, 5])
+    assert :notfound == Katas.jmp_search(4, [1, 3, 5])
+    assert :notfound == Katas.jmp_search(6, [1, 3, 5])
+
+    # assert :notfound == Katas.interpolation_search(0, [1, 3, 5])
+    # assert :notfound == Katas.interpolation_search(2, [1, 3, 5])
+    # assert :notfound == Katas.interpolation_search(4, [1, 3, 5])
+    # assert :notfound == Katas.interpolation_search(6, [1, 3, 5])
 
     assert :notfound == Katas.lin_search(0, [1, 3, 5, 7])
     assert :notfound == Katas.lin_search(2, [1, 3, 5, 7])
@@ -64,11 +91,24 @@ defmodule KatasTest do
     assert :notfound == Katas.lin_search(6, [1, 3, 5, 7])
     assert :notfound == Katas.lin_search(8, [1, 3, 5, 7])
 
-    assert :notfound == Katas.chop(0, [1, 3, 5, 7])
-    assert :notfound == Katas.chop(2, [1, 3, 5, 7])
-    assert :notfound == Katas.chop(4, [1, 3, 5, 7])
-    assert :notfound == Katas.chop(6, [1, 3, 5, 7])
-    assert :notfound == Katas.chop(8, [1, 3, 5, 7])
+    assert :notfound == Katas.bin_search(0, [1, 3, 5, 7])
+    assert :notfound == Katas.bin_search(2, [1, 3, 5, 7])
+    assert :notfound == Katas.bin_search(4, [1, 3, 5, 7])
+    assert :notfound == Katas.bin_search(6, [1, 3, 5, 7])
+    assert :notfound == Katas.bin_search(8, [1, 3, 5, 7])
+
+    assert :notfound == Katas.jmp_search(0, [1, 3, 5, 7])
+    assert :notfound == Katas.jmp_search(2, [1, 3, 5, 7])
+    assert :notfound == Katas.jmp_search(4, [1, 3, 5, 7])
+    assert :notfound == Katas.jmp_search(6, [1, 3, 5, 7])
+    assert :notfound == Katas.jmp_search(8, [1, 3, 5, 7])
+
+    # assert :notfound == Katas.interpolation_search(0, [1, 3, 5, 7])
+    # assert :notfound == Katas.interpolation_search(2, [1, 3, 5, 7])
+    # assert :notfound == Katas.interpolation_search(4, [1, 3, 5, 7])
+    # assert :notfound == Katas.interpolation_search(6, [1, 3, 5, 7])
+    # assert :notfound == Katas.interpolation_search(8, [1, 3, 5, 7])
+
   end
 
 end
